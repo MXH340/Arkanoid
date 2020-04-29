d9f574ea61687e4807b5562191108a58181cf56b
mengx <1264235380@qq.com>
2020-04-29 20:24:13 +0800

    增加平台控制移动与碰撞

--- Arkanoid/main.cpp
+++ Arkanoid/main.cpp
@@ -11,12 +11,14 @@ void draw(RenderWindow& app);
 void CreateBlocks();
 void Move();
 bool isCollide(Sprite s1, Sprite s2);
+void Control();
 
 int main()
 {
 	RenderWindow app(VideoMode(520, 450), "Arknoid Game!");
 	app.setFramerateLimit(60);
 	ball.setPosition(300, 300);
+	paddle.setPosition(300, 440);
 	CreateBlocks();
 	while (app.isOpen())
 	{
@@ -26,6 +28,7 @@ int main()
 			if (e.type == Event::Closed)
 				app.close();
 		}
+		Control();
 		Move();
 		draw(app);
 	}
@@ -45,7 +48,7 @@ void draw(RenderWindow& app)
 
 	ball.setTexture(t3);
 	paddle.setTexture(t4);
-	paddle.setPosition(300, 440);
+
 	app.clear();
 	app.draw(bk);
 	app.draw(paddle);
@@ -75,6 +78,7 @@ void CreateBlocks()
 //ÇòµÄÒÆ¶¯
 void Move()
 {
+	srand(time(0));
 	ball.move(dx, 0);
 	for (int i = 0; i < N; i++)
 	{
@@ -98,10 +102,20 @@ void Move()
 		dx = -dx;
 	if (ballPos.y < 0 || ballPos.y>450)
 		dy = -dy;
+	if (isCollide(ball, paddle))
+		dy = -(rand() % 5 + 2);
 }
 
 //¼ì²âÅö×²
 bool isCollide(Sprite s1, Sprite s2)
 {
 	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
+
+void Control()
+{
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+		paddle.move(6, 0);
+	else if (Keyboard::isKeyPressed(Keyboard::Left))
+		paddle.move(-6, 0);
 }
